cd9f02bea31f8240792a559788c810f0dd6e7d28
Amanda Ackerman <ackerm44@gmail.com>
2017-10-03 13:55:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var recipe = new Object({eggs: 3});
+var recipes = new Object({eggs: 3});
 
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key]:value})
