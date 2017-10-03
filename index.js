d13a8b70a45af0765fb6b6872218fb75b31e334b
Amanda Ackerman <ackerm44@gmail.com>
2017-10-03 13:52:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,14 @@ function updateObjectWithKeyAndValue(object, key, value) {
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object.key = value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key) {
+  delete object[key];
   return object; 
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+
 }
